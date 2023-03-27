5071c803460ebadf2f03b3f483b60eadc786e1a7
FPirazz <federico.pirazzoli@studio.unibo.it>
2023-03-27 11:37:08 +0200

    Tested Equals and ToString methods (Not working)

--- src/main/scala/u04lab/code/SecondDegreePolynomial.scala
+++ src/main/scala/u04lab/code/SecondDegreePolynomial.scala
@@ -15,7 +15,8 @@ object SecondDegreePolynomial:
   private class SecondDegreePolynomialImpl(override val constant: Double,
                                    override val firstDegree: Double,
                                    override val secondDegree: Double) extends SecondDegreePolynomial:
-    override def +(polynomial: SecondDegreePolynomial): SecondDegreePolynomial = ???
+    override def +(polynomial: SecondDegreePolynomial): SecondDegreePolynomial = polynomial match
+      case impl: SecondDegreePolynomialImpl => ???
     override def -(polynomial: SecondDegreePolynomial): SecondDegreePolynomial = ???
 
 
@@ -28,11 +29,12 @@ object SecondDegreePolynomial:
   val multipleOperations = fullPolynomial - (anotherPolynomial + simplePolynomial)
   println((multipleOperations, multipleOperations.secondDegree, multipleOperations.firstDegree, multipleOperations.constant)) // 2.0 * X^2 + 1.0 * X + 2.0
 
-@main def testSum(): Unit =
+@main def testEqualAndString(): Unit =
   val simplePolynomial = SecondDegreePolynomial(1.0, 0, 3)
   val anotherPolynomial = SecondDegreePolynomial(0.0, 1, 0.0)
-  val sum = simplePolynomial + anotherPolynomial
-  println(simplePolynomial + anotherPolynomial)
+  println(simplePolynomial.toString)
+  println(simplePolynomial.equals(anotherPolynomial))
+
 
 /** Hints:
   *   - implement SecondDegreePolynomial with a SecondDegreePolynomialImpl class, similar to PersonImpl in slides
