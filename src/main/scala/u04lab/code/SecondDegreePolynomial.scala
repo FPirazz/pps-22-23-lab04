b27e8909b8d1d67e418ed7835e7b28e1804d93ab
FPirazz <federico.pirazzoli@studio.unibo.it>
2023-03-27 11:56:31 +0200

    Implemented Exercise 1 (Finished implementing operations plus and minus)

--- src/main/scala/u04lab/code/SecondDegreePolynomial.scala
+++ src/main/scala/u04lab/code/SecondDegreePolynomial.scala
@@ -11,13 +11,14 @@ trait SecondDegreePolynomial:
 
 object SecondDegreePolynomial:
   def apply(secondDegree: Double, firstDegree: Double, constant: Double): SecondDegreePolynomial =
-    SecondDegreePolynomialImpl(constant, firstDegree, secondDegree)// Fill here
-  private case class SecondDegreePolynomialImpl(override val constant: Double,
+    SecondDegreePolynomialImpl(secondDegree, firstDegree, constant)// Fill here
+  private case class SecondDegreePolynomialImpl(override val secondDegree: Double,
                                    override val firstDegree: Double,
-                                   override val secondDegree: Double) extends SecondDegreePolynomial:
-    override def +(polynomial: SecondDegreePolynomial): SecondDegreePolynomial = polynomial match
-      case impl: SecondDegreePolynomialImpl => ???
-    override def -(polynomial: SecondDegreePolynomial): SecondDegreePolynomial = ???
+                                   override val constant: Double) extends SecondDegreePolynomial:
+    override def +(polynomial: SecondDegreePolynomial): SecondDegreePolynomial =
+      SecondDegreePolynomial(secondDegree + polynomial.secondDegree, firstDegree + polynomial.firstDegree, constant + polynomial.constant )
+    override def -(polynomial: SecondDegreePolynomial): SecondDegreePolynomial =
+      SecondDegreePolynomial(secondDegree - polynomial.secondDegree, firstDegree - polynomial.firstDegree, constant - polynomial.constant)
 
 
 @main def checkComplex(): Unit =
