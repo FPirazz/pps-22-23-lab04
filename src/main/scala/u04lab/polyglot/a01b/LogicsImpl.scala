0d0badf7c9e4fe43db9d5496a8c605e5fea7caf4
FPirazz <federico.pirazzoli@studio.unibo.it>
2023-03-28 20:41:58 +0200

    Implemented hit function with win case

--- src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
@@ -12,14 +12,21 @@ import scala.util.Random
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01b/sol2/ */
 class LogicsImpl(private val size: Int, private val mines: Int) extends Logics:
   private val random = Random(42)
+  private var revealedCells: Int = 0
   private var _minesPos: List[(Int, Int)] = Stream.toList(
     Stream.take(
       Stream.iterate((random.nextInt(size), random.nextInt(size)))((_, _) => (random.nextInt(size), random.nextInt(size)))
     )(mines)
   )
+  def hit(x: Int, y: Int): java.util.Optional[Integer] = (x, y) match
+    case (_, _) if List.contains(_minesPos, (x, y)) => OptionToOptional(None()) // Option => Optional converter
+    case _ => var count: Int = 0;
+              for
+                i <- x-1 to x+1
+                j <- y-1 to y+1
+              do if List.contains(_minesPos, (i, j)) then  count = count + 1
+              revealedCells = revealedCells + 1
+              if revealedCells == (size * size) - mines then won = true
+              OptionToOptional(Some(count))
 
-  def hit(x: Int, y: Int): java.util.Optional[Integer] =
-    println(_minesPos)
-    OptionToOptional(None()) // Option => Optional converter
-
-  def won = false
+  var won = false
